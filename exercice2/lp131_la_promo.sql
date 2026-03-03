cc743042a014100de161edaf6eccad83fd18ffdd
Kembellec <geraldkembellec@gmail.com>
2026-03-03 15:11:31 +0100

    Update lp131_la_promo.sql

--- exercice2/lp131_la_promo.sql
+++ exercice2/lp131_la_promo.sql
@@ -1,21 +1,10 @@
--- phpMyAdmin SQL Dump
--- version 5.2.3
--- https://www.phpmyadmin.net/
---
--- Hôte : localhost:8889
--- Généré le : mar. 03 mars 2026 à 14:07
--- Version du serveur : 8.0.44
--- Version de PHP : 8.3.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/* Il faut d'abord créer la base avant d'insérer les tables */;
 
 --
 -- Base de données : `lp131_26`
