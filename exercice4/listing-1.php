ef11f7b36332822b0ed98616c0b3d7507d860df9
Kembellec <geraldkembellec@gmail.com>
2026-03-17 14:09:55 +0100

    Update listing-1.php

--- exercice4/listing-1.php
+++ exercice4/listing-1.php
@@ -1,1 +1,14 @@
-
+<html>
+  <body>
+    <?php
+    /* création du connecteur vers la base de données lp131_26 */
+    $connecteur=mysqli_connect("localhost","root","root","lp131_26");
+    $resultat=mysqli_query($connecteur,"SELECT * FROM promo");
+    ?>
+  <pre>
+    <?php
+    while($row=mysqli_fetch_assoc($resultat))var_dump($row);
+    ?>
+  </pre>
+  </body>
+</html>
